46733ee47ebcec98a706072c19fd7e52484e042a
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2016-03-21 19:34:53 -0300

    Update main.cpp

--- histogram/histogram/main.cpp
+++ histogram/histogram/main.cpp
@@ -5,13 +5,63 @@
 using namespace cv;
 using namespace std;
 
+int transformation(int A /*This A=(L-1)/MN*/, int Array_Ocurrrencies[], int k){
+    int sum;
+    for(int i=0; i<=k; i++){
+        sum = sum + Array_Ocurrrencies[i];
+    }
+
+    return sum;
+}
+
+void equalize_image (Mat image, int L){
+
+    int R[64]={}, G[64]={}, B[64]={}; //Each array will have the index as the Intensity and the value as the occurency
+    int height = image.size().height;
+    int width = image.size().width;
+    Vec3b intensity; // This vector will be used to  store intensity values of the three channels
+
+    //First, let's build the histogram array
+
+    for(int i=0; i<height;i++)
+    {
+        for(int j=0; j<width; j++)
+        {
+            intensity = image.at<Vec3b>(i,j);
+
+            B[int((intensity.val[0]))/4]++;
+            G[int((intensity.val[1]))/4]++;
+            R[int((intensity.val[2]))/4]++;
+        }
+    }
+
+    //Second, apply the transformation in each pixel
+
+    float A = (float(L)-1.0)/(float(height*width));
+
+    for(int i=0; i<height;i++)
+    {
+        for(int j=0; j<width; j++)
+        {
+            intensity = image.at<Vec3b>(i,j);
+
+            intensity.val[0] = transformation(A, B, intensity.val[0]);
+            intensity.val[1] = transformation(A, G, intensity.val[1]);
+            intensity.val[2] = transformation(A, R, intensity.val[2]);
+
+            image.at<Vec3b>(i,j) = intensity;
+        }
+    }
+}
+
+
 int main(int argc, char** argv){
   Mat image;
   int width, height;
   VideoCapture cap;
   vector<Mat> planes;
   Mat histR, histG, histB;
-  int nbins = 64;
+  int nbins = 64; // This is the L of our equation
   float range[] = {0, 256};
   const float *histrange = { range };
   bool uniform = true;
@@ -37,7 +87,11 @@ int main(int argc, char** argv){
 
   while(1){
     cap >> image;
+
+    equalize_image(image, nbins);
+
     split (image, planes);
+
     calcHist(&planes[0], 1, 0, Mat(), histR, 1,
              &nbins, &histrange,
              uniform, acummulate);
